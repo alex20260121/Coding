d4bcc025ffdb312ed0c62ed1ecfa1a2c28dd2218
alice <alice20250825@gmail.com>
2026-05-02 13:49:59 +0800

    update

--- CloudOS/cluster_exporter.py
+++ CloudOS/cluster_exporter.py
@@ -1,40 +1,63 @@
+#!/usr/bin/env python3.13
+
 # -*- coding: utf-8 -*-
+"""
+Documentation:
+Cloud OS 5.0 Component Service Exporter: Exports corresponding cluster information based on the platform's component services to a CSV file.
+cluster_exporter.py [serverName]
+    serverName: Pass in the component service name.
+"""
 
 import os
+import sys
 import json
-import csv
-import time
+from csv import DictWriter as dw
+from time import strftime as sftm, localtime as lt
 
-json_root = os.path.join(os.path.dirname(__file__), "JSON")
-json_file = os.path.join(json_root, "redis_cluster_details.json")
+if len(sys.argv) != 2:
+    print(__doc__)
+    sys.exit(1)
 
-def convert_data(filename, raw=False, filter=False) -> list:
-    with open(file=filename, mode='r', encoding='utf-8') as f:
+server_name = sys.argv[1]
+
+json_data_root = os.path.join(os.path.dirname(__file__), 'json')
+if not os.path.exists(json_data_root):
+    os.mkdir(json_data_root)
+
+csv_data_root = os.path.join(os.path.dirname(__file__), 'csv')
+if not os.path.exists(csv_data_root):
+    os.mkdir(csv_data_root)
+
+def convert_python_data():
+    json_file_name = os.path.join(json_data_root, f'{server_name}_cluster_details.json')
+    with open(file=json_file_name, mode='r', encoding='utf-8') as f:
         pydata = json.load(f)
-    if raw:
-        return pydata
-
-    if filter:
-        filtered = [ {k: v for k, v in inner['data'].items() if v is not None and v != ""} for inner in pydata ]
-        return filtered
-    else:
-        inner_data = [item['data'] for item in pydata]
-        return inner_data
-
-def write_in_csv(data: list):
+    for item in pydata:
+        yield(item['data'])
+    
+def all_top_key():
     top_level_key = []
     seen = set()
-    for item in data:
-        for k in item:
-            if k not in seen:
-                seen.add(k)
-                top_level_key.append(k)
-    instance_key = ["availabilityZone", "instanceName", "internalIp", "cpu", "ram", "disk"]
-    headers = top_level_key + instance_key
-    target_csv_path = os.path.join(os.path.dirname(__file__), f"redis_cluster_export_{time.strftime("%Y%m%d%H%M%S", time.localtime())}.csv")
-    with open(target_csv_path, mode='w', newline='', encoding='utf-8-sig') as f:
-        writer = csv.DictWriter(f, fieldnames=headers)
-        writer.writeheader()
+    for data in convert_python_data():
+        for key in data:
+            if key not in seen:
+                seen.add(key)
+                top_level_key.append(key)
+    return top_level_key
+
+def csv_writer(default: bool = True, *args):
+    csv_file_name = os.path.join(csv_data_root, f'{server_name}_cluster_{sftm("%Y%m%d%H%M%S", lt())}.csv')
+    data = [x for x in convert_python_data()]
+    if default:
+        top_key = all_top_key()
+        with open(file=csv_file_name, mode='w', newline='', encoding='utf-8') as f:
+            csv_writer = dw(f, fieldnames=top_key)
+            csv_writer.writeheader()
+            csv_writer.writerows(data)
+        promt = f"Write to the CSV file using the default mode({csv_file_name})."
+        return promt
 
 
-print(write_in_csv(convert_data(json_file, filter=True)))
+if __name__ == "__main__":
+    result = csv_writer()
+    print(result)
